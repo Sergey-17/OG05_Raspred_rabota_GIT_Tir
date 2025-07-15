7df5f4ddf0dbffc08503b56ee008a70d9eae18bb
Sergey-17 <gsaposte@gmail.com>
2025-07-16 00:53:18 +0900

    11

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ import pygame
 import random
 
 pygame.init()
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1800
+SCREEN_HEIGHT = 1200
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра Тир")
 icon = pygame.image.load("img/icon.png")
 pygame.display.set_icon(icon)
 
-target_img = pygame.image.load("img/target.jpg")
-target_width = 80
-target_height = 80
+target_img = pygame.image.load("img/icon.png")
+target_width = 220
+target_height = 220
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
@@ -21,6 +21,11 @@ color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 running = True
 while running:
-        pass
+        screen.fill(color)
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                        running = False
+        screen.blit(target_img, (target_x, target_y))
+        pygame.display.update()
 
 pygame.quit()
