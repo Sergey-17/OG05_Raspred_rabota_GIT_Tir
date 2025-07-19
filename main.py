e3f0d6a5de76727b3340246185f1685f12a06c9d
Sergey-17 <gsaposte@gmail.com>
2025-07-19 17:43:52 +0900

    add сycle сode

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ import pygame
 import random
 
 pygame.init()
-SCREEN_WIDTH = 1800
-SCREEN_HEIGHT = 1200
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра Тир")
 icon = pygame.image.load("img/icon.png")
 pygame.display.set_icon(icon)
 
-target_img = pygame.image.load("img/icon.png")
-target_width = 220
-target_height = 220
+target_img = pygame.image.load("img/target.png")
+target_width = 100
+target_height = 100
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
@@ -25,6 +25,13 @@ while running:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                         running = False
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                        target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                        target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
         screen.blit(target_img, (target_x, target_y))
         pygame.display.update()
 
